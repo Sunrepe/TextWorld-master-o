ad126cb47e3bc75818267b7fea49728847985b6c
zxf <bnuzxf@163.com>
2022-04-26 09:35:21 +0800

    删除world

--- textworld/generator/inform7/world2inform7.py
+++ textworld/generator/inform7/world2inform7.py
@@ -459,14 +459,6 @@ class Inform7Game:
             say "                      | $$   | $$     \| $$  | $$   | $$          [line break]";
             say "                       \$$    \$$$$$$$$ \$$   \$$    \$$          [line break]";
             say "              __       __   ______   _______   __        _______  [line break]";
-            say "             |  \  _  |  \ /      \ |       \ |  \      |       \ [line break]";
-            say "             | $$ / \ | $$|  $$$$$$\| $$$$$$$\| $$      | $$$$$$$\[line break]";
-            say "             | $$/  $\| $$| $$  | $$| $$__| $$| $$      | $$  | $$[line break]";
-            say "             | $$  $$$\ $$| $$  | $$| $$    $$| $$      | $$  | $$[line break]";
-            say "             | $$ $$\$$\$$| $$  | $$| $$$$$$$\| $$      | $$  | $$[line break]";
-            say "             | $$$$  \$$$$| $$__/ $$| $$  | $$| $$_____ | $$__/ $$[line break]";
-            say "             | $$$    \$$$ \$$    $$| $$  | $$| $$     \| $$    $$[line break]";
-            say "              \$$      \$$  \$$$$$$  \$$   \$$ \$$$$$$$$ \$$$$$$$ [line break]";
             say "[variable letter spacing][line break]";
             say "[objective][line break]".
 
